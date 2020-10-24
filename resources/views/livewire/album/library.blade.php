96ec1b40a902ede5078b61e3dbb47b772c917128
Djillali <ch.djillali@gmail.com>
2020-10-24 18:55:20 -0400

    Welcome page - WIP

--- resources/views/livewire/album/library.blade.php
+++ resources/views/livewire/album/library.blade.php
@@ -5,8 +5,8 @@
 
     <div class="py-4">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-		        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div class="bg-gray-400 overflow-hidden shadow-xl sm:rounded-lg">
+		        <div class="p-6 sm:px-20 bg-gray-400 border-b border-gray-200">
 
 				    <form wire:submit.prevent="submitForm" class="mb-6" method="POST" action="/music/tracks">
 					@csrf
