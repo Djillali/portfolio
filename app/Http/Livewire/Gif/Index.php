10028559bf86ad306fb0c4921f2d488157a06269
Djillali <ch.djillali@gmail.com>
2020-10-26 13:01:34 -0400

    Gif library now searchable by title and by tag - done

--- app/Http/Livewire/Gif/Index.php
+++ app/Http/Livewire/Gif/Index.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Gif;
 
 use App\Models\Gif;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -17,7 +18,11 @@ class Index extends Component
                         ->paginate(10);
 
         return view('livewire.gif.index', [
-        	'gifs' => Gif::where('title','like','%' . $this->search . '%')->paginate(4),
+            'gifs' => Gif::where('title','like','%' . $this->search . '%')->orWhereHas('gifTags', function (Builder $query) {
+                $query->whereHas('tag', function(Builder $query2) {
+                    $query2->where('tag','like','%' . $this->search . '%');
+                });
+            })->orderBy('created_at','desc')->paginate(4),
         ]);
     }
 }
